65561b1f9f2336b8ca5833c6315334cedf4ce934
sawwere <sawer_13@mail.ru>
2024-02-29 12:53:50 +0300

    Add annulling cards

--- ExpirationManagerWeb/src/main/java/com/mycompany/ExpirationManagerWeb/controllers/CardController.java
+++ ExpirationManagerWeb/src/main/java/com/mycompany/ExpirationManagerWeb/controllers/CardController.java
@@ -2,8 +2,9 @@ package com.mycompany.ExpirationManagerWeb.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mycompany.ExpirationManagerWeb.models.Card;
-import com.mycompany.ExpirationManagerWeb.models.Client;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,11 @@ public class CardController {
     public static final String GET_CARDS = "/cards";
     public static final String CREATE_CARD = "/clients/{client_id}/cards";
 
+    public static final String ANNUL_CARD = "/cards/{card_id}/status/annul";
+
     @GetMapping(GET_CARDS)
     public ModelAndView index() throws Exception{
-        ModelAndView mav =new ModelAndView("cards");
+        ModelAndView mav = new ModelAndView("cards");
         RestClient defaultClient = RestClient.create();
         String result = defaultClient.get()
                 .uri("http://localhost:8080/api/cards")
@@ -37,10 +40,23 @@ public class CardController {
             Card card) throws Exception{
         RestClient defaultClient = RestClient.create();
         String result = defaultClient.post()
-                .uri("http://localhost:8080/api/clients/%s/cards".formatted(clientId))
+                .uri("http://localhost:8080/api/clients/%d/cards".formatted(clientId))
                 .body(card)
                 .retrieve()
                 .body(String.class);
         return "redirect:%s".formatted(ClientController.SHOW_CLIENT);
+    }
+
+    @GetMapping(ANNUL_CARD)
+    public String annulCard(@PathVariable(value = "card_id") Long cardId,
+                            HttpServletRequest request) {
+        RestClient defaultClient = RestClient.create();
+        String result = defaultClient.put()
+                .uri("http://localhost:8080/api/cards/%d/status".formatted(cardId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{status\" : \"ANNULLED\"}")
+                .retrieve()
+                .body(String.class);
+        return "redirect:" + request.getHeader("Referer");
     }
 }
